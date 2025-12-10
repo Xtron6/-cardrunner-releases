6bd7b0adfb0df546428fc98ac5ddcc41a8ef3d28
Xavier Gallo <xavigallo@gmail.com>
2025-12-10 12:40:27 -0500

    Initial Commit

---
+++ TheOffloader/TheOffloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheOffloaderApp.swift
+//  TheOffloader
+//
+//  Created by Xavier Gallo on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TheOffloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheOffloaderTests/TheOffloaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  TheOffloaderTests.swift
+//  TheOffloaderTests
+//
+//  Created by Xavier Gallo on 12/10/25.
+//
+
+import Testing
+
+struct TheOffloaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
